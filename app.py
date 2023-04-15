9df4418b8dec6c80710a56fd3eb9d13a1af21ab5
Akshaykushawaha <akshay.kushawaha@gmail.com>
2023-04-15 20:59:32 +0530

    updated some exceptional cases

--- app.py
+++ app.py
@@ -5,19 +5,19 @@ from datacl import get_azure_data,len_of_excel,write_to_excel,getdata
 app = Flask("__name__")
 
 @app.route('/fdata')
-def fdata_func():
-    row = get_azure_data()
-    len_excel = len_of_excel()
-    
-    write_to_excel(len_excel,row)
-    upd_time=getdata("time")
+def fdata_func():                       # full data refresh
+    row = get_azure_data()              # get data from azure in a row
+    len_excel = len_of_excel()          # get number of last row in excel
+    if (row!=[]):                       # check if data was avaialabe from azure
+        write_to_excel(len_excel,row)   # updating excel
+    upd_time=getdata("time")            # getting last upadte time from excel
     d={}
-    d["val"] = upd_time
-    return jsonify(d)
+    d["val"] = upd_time                 # saving it in dict
+    return jsonify(d)                   # returning the value
 
 @app.route('/odata',methods=['GET'])
-def odata_func():
-    upd_time=getdata(str(request.args["sensor"]))
+def odata_func():                                  #only data fetch
+    upd_time=getdata(str(request.args["sensor"]))  # getting last upadte value of that sensor from excel
     d={}
     d["val"] = upd_time
     return jsonify(d)
@@ -25,4 +25,3 @@ def odata_func():
 
 if __name__=="__main__":
     app.run(port=32)
-    
--- datacl.py
+++ datacl.py
@@ -9,30 +9,35 @@ def get_azure_data():
 
 def len_of_excel():     
         
-    if (exists('hello.xlsx')==False):
+    if (exists('data.xlsx')==False):
+        import openpyxl
         wb = openpyxl.Workbook() 
         sheet = wb['Sheet']
         sheet.title = 'Sheet1'
-        wb.save(filename='hello.xlsx')
+        row=1
+        new_row = ['temp','humidity','light','soil','water','npk','time']
+        for col, entry in enumerate(new_row, start=1):
+            sheet.cell(row=row, column=col, value=entry)
+        wb.save(filename='data.xlsx')
         
-    dataframe1 = pd.read_excel('hello.xlsx')
+    dataframe1 = pd.read_excel('data.xlsx')
     len_excel=len(dataframe1['time'])
     return len_excel
 
 def write_to_excel(len_excel,new_row):
 
-    workbook = openpyxl.load_workbook(filename='hello.xlsx')
+    workbook = openpyxl.load_workbook(filename='data.xlsx')
     worksheet = workbook['Sheet1'] 
     
     row=len_excel+2
     for col, entry in enumerate(new_row, start=1):
         worksheet.cell(row=row, column=col, value=entry)
 
-    workbook.save('hello.xlsx')
+    workbook.save('data.xlsx')
 
 def getdata(cl_name):
     # read by default 1st sheet of an excel file
-    dataframe1 = pd.read_excel('hello.xlsx')
+    dataframe1 = pd.read_excel('data.xlsx')
     cl_data = list(dataframe1[cl_name])
     data=cl_data[-1]
     return data
